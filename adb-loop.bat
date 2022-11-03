e0f7f675d5649bf76c263937de67b641d701b221
Rellik Jaeger <rellikjaeger@gmail.com>
2022-11-03 17:08:13 +0630

    Add files via upload

---
+++ adb-loop.bat
@@ -0,0 +1,32 @@
+@echo off
+title Force ADB over WiFi TCP^/IP
+setlocal EnableDelayedExpansion
+echo Press Ctrl+C and press y to Terminate
+set /p ip=Enter your IP: 
+:x
+adb devices >nul
+for /f %%f in ('adb devices ^| grep "!ip!:5555"') do (
+	if [%%f] neq [!ip!:5555] goto :next
+	if [%%f] equ [!ip!:5555] (
+		echo.
+		echo Connected: %%f
+		goto :eof
+	)
+)
+:next
+echo.
+echo Waiting for a USB connection...
+echo.
+adb tcpip 5555
+adb connect !ip!
+for /f %%f in ('adb devices ^| grep "!ip!:5555"') do (
+	if [%%f] equ [!ip!:5555] (
+		echo.
+		echo Connected: %%f
+		goto :eof
+	)
+)
+timeout /t 1 /nobreak
+goto :x
+
+:eof
